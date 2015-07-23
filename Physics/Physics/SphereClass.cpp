5ddf8c812ade21f1d2a09b403f91839e142f1fec
Andrew <andrew.giannopoulos@gmail.com>
2015-07-23 11:30:33 +1000

    Updated SphereClass for 3D

--- Physics/Physics/SphereClass.cpp
+++ Physics/Physics/SphereClass.cpp
@@ -1,7 +1,7 @@
 #include "SphereClass.h"
 #include <Gizmos.h>
 
-SphereClass::SphereClass(glm::vec2 position, glm::vec2 velocity, float mass, float radius, glm::vec4 colour) : RigidBody(position, velocity, 0, mass)
+SphereClass::SphereClass(glm::vec3 position, glm::vec3 velocity, float mass, float radius, glm::vec4 colour) : RigidBody(position, velocity, 0, mass)
 {
 	m_radius = radius;
 	m_colour = colour;
@@ -14,7 +14,7 @@ SphereClass::~SphereClass()
 
 void SphereClass::MakeGizmo()
 {
-	Gizmos::addSphere(glm::vec3(m_position, 0), m_radius, 20, 20, m_colour);
+	Gizmos::addSphere(m_position, m_radius, 20, 20, m_colour);
 }
 
 void SphereClass::Debug()
