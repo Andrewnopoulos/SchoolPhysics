2fd4cec602cff3bb5fe37826364e85f2f4fef702
Andrew <andrew.giannopoulos@gmail.com>
2015-07-23 11:38:31 +1000

    Updated RigidBody to work with 3D Physics

--- Physics/Physics/RigidBody.cpp
+++ Physics/Physics/RigidBody.cpp
@@ -5,7 +5,7 @@ RigidBody::~RigidBody()
 
 }
 
-RigidBody::RigidBody(glm::vec2 position, glm::vec2 velocity, float rotation, float mass)
+RigidBody::RigidBody(glm::vec3 position, glm::vec3 velocity, float rotation, float mass)
 {
 	m_position = position;
 	m_velocity = velocity;
@@ -13,18 +13,18 @@ RigidBody::RigidBody(glm::vec2 position, glm::vec2 velocity, float rotation, flo
 	m_mass = mass;
 }
 
-void RigidBody::applyForce(glm::vec2 force)
+void RigidBody::applyForce(glm::vec3 force)
 {
 	m_velocity += force / m_mass;
 }
 
-void RigidBody::applyForceToActor(RigidBody* actor2, glm::vec2 force)
+void RigidBody::applyForceToActor(RigidBody* actor2, glm::vec3 force)
 {
 	actor2->applyForce(-force);
 	applyForce(force);
 }
 
-void RigidBody::Update(glm::vec2 gravity, float timeStep)
+void RigidBody::Update(glm::vec3 gravity, float timeStep)
 {
 	m_velocity += gravity * timeStep;
 	m_position += m_velocity * timeStep;
