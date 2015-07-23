1a7f16deff29d1d1b954c3044900e1dd001d2aa6
Andrew <andrew.giannopoulos@gmail.com>
2015-07-23 11:39:48 +1000

    Updated Plane to work with 3D

--- Physics/Physics/Plane.cpp
+++ Physics/Physics/Plane.cpp
@@ -1,7 +1,7 @@
 #include "Plane.h"
 #include <Gizmos.h>
 
-Plane::Plane(glm::vec2 normal, float distanceToOrigin)
+Plane::Plane(glm::vec3 normal, float distanceToOrigin)
 {
 	m_normal = glm::normalize(normal);
 	m_distanceToOrigin = distanceToOrigin;
@@ -15,7 +15,7 @@ Plane::~Plane()
 
 void Plane::MakeGizmo()
 {
-	glm::vec2 point1 = (m_normal * m_distanceToOrigin) + (glm::vec2(m_normal.y, -m_normal.x) * 100.0f);
-	glm::vec2 point2 = (m_normal * m_distanceToOrigin) + (glm::vec2(-m_normal.y, m_normal.x) * 100.0f);
-	Gizmos::addLine(glm::vec3(point1, 0), glm::vec3(point2, 0), glm::vec4(1, 1, 1, 1));
+	glm::vec3 point1 = (m_normal * m_distanceToOrigin) + (glm::vec3(m_normal.y, -m_normal.x, 0) * 100.0f);
+	glm::vec3 point2 = (m_normal * m_distanceToOrigin) + (glm::vec3(-m_normal.y, m_normal.x, 0) * 100.0f);
+	Gizmos::addLine(point1, point2, glm::vec4(1, 1, 1, 1));
 }
